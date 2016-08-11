3dcc6fef379983bdb3165c0ec914a6bb63b5f58a
Charlene <charlenec11@yahoo.com.au>
2016-08-11 21:59:32 +0100

    Add fragments and view pager

---
+++ app/src/main/java/com/example/android/touristapp/CategoryAdapter.java
@@ -0,0 +1,53 @@
+package com.example.android.touristapp;
+
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+/**
+ * Created by charlene on 10/08/16.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    /** Context of the app */
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_museums);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_theatres);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_attractions);
+        } else {
+            return mContext.getString(R.string.category_parks);
+        }
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new MuseumsFragment();
+        } else if (position == 1) {
+            return new TheatresFragment();
+        } else if (position == 2) {
+            return new AttractionsFragment();
+        } else {
+            return new ParksFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
--- app/src/main/java/com/example/android/touristapp/MainActivity.java
+++ app/src/main/java/com/example/android/touristapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.touristapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -12,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
 
 
         // Find the View that shows the museums category
