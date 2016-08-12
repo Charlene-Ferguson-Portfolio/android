35db31dc5dd7bfb430648a2526942c6565aeae2b
Charlene <charlenec11@yahoo.com.au>
2016-08-12 22:52:19 +0100

    Add arraylist with content for listviews

--- app/src/main/java/com/example/android/touristapp/AttractionsFragment.java
+++ app/src/main/java/com/example/android/touristapp/AttractionsFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class AttractionsFragment extends Fragment {
@@ -19,8 +22,33 @@ public class AttractionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_attractions, container, false);
+        /// Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.interest_list, container, false);
+
+        final ArrayList<PointOfInterest> interests = new ArrayList<>();
+
+        interests.add(new PointOfInterest("British Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Science Museum",R.mipmap.ic_launcher ));
+        interests.add(new PointOfInterest("Natural History Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Victoria and Albert Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Tate Modern", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("London Transport Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("National Gallery",  R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Design Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Churchill War Rooms",  R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Charles Dickens", R.mipmap.ic_launcher));
+
+        // Create an adapter whose data source is a list of musuems. The
+        // adapter knows how to create list items for each item in the list.
+        PointOfInterestAdapter adapter = new PointOfInterestAdapter(getActivity(), interests, R.color.category_museums);
+
+        //Create a list view
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        // Set the adapter for the list view
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 
 
--- app/src/main/java/com/example/android/touristapp/TheatresFragment.java
+++ app/src/main/java/com/example/android/touristapp/TheatresFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TheatresFragment extends Fragment {
 
@@ -19,7 +22,33 @@ public class TheatresFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_theatres, container, false);
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.interest_list, container, false);
+
+        final ArrayList<PointOfInterest> interests = new ArrayList<>();
+
+        interests.add(new PointOfInterest("British Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Science Museum",R.mipmap.ic_launcher ));
+        interests.add(new PointOfInterest("Natural History Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Victoria and Albert Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Tate Modern", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("London Transport Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("National Gallery",  R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Design Museum", R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Churchill War Rooms",  R.mipmap.ic_launcher));
+        interests.add(new PointOfInterest("Charles Dickens", R.mipmap.ic_launcher));
+
+        // Create an adapter whose data source is a list of musuems. The
+        // adapter knows how to create list items for each item in the list.
+        PointOfInterestAdapter adapter = new PointOfInterestAdapter(getActivity(), interests, R.color.category_museums);
+
+        //Create a list view
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        // Set the adapter for the list view
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 
 
